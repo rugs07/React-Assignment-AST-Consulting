09a33bdb0f6af371f8b335a575598a9016fab907
Rugwed Bodhankar <rugwed07@gmail.com>
2024-02-13 23:35:41 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { GoogleLogin } from 'react-google-login';
-import axios from 'axios';
-import Map from './Map';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { GoogleLogin } from "react-google-login";
+import axios from "axios";
+import Map from "./Map";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Navigate } from 'react-router'
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -27,10 +28,12 @@ const App = () => {
 
   const handleLoginSuccess = (response) => {
     setLoggedIn(true);
+    console.log("Login successfull");
+    
   };
 
   const handleLoginFailure = (response) => {
-    console.log(response);
+    console.log("Error", response);
   };
 
   const handleMarkerClick = (hospital) => {
@@ -45,31 +48,32 @@ const App = () => {
     if (currentLocation) {
       axios
         .get(
-          `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${currentLocation.lat},${currentLocation.lng}&radius=5000&type=hospital&key=AIzaSyCsQg0fqJYUTKDyQwME9qR77gsh6eYZO8I`
+          `https://api.geoapify.com/v2/places?categories=healthcare.hospital&filter=circle:${currentLocation.lng},${currentLocation.lat}&limit=20&apiKey=024620af7b884a94b4d0a1a2225c7985`
         )
         .then((response) => {
           setHospitals(response.data.results);
+          console.log(response.data)
         })
         .catch((error) => {
-          console.log(error);
+          console.log('hello',error);
         });
     }
   }, [currentLocation]);
 
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         {!loggedIn ? (
           <GoogleLogin
-            clientId="204320152383-k8kfhplumfsjrdllug1gf83036jr4hul.apps.googleusercontent.com"
+            clientId="204320152383-0r47fhllms5nrrg0hd2r63i3c2v9lfdt.apps.googleusercontent.com"
             buttonText="Login with Google"
             onSuccess={handleLoginSuccess}
             onFailure={handleLoginFailure}
-            cookiePolicy={'single_host_origin'}
+            cookiePolicy={"single_host_origin"}
           />
         ) : (
           <>
-            <Route exact path="/">
+            <Navigate to='/user'/>
               <Map
                 currentLocation={currentLocation}
                 hospitals={hospitals}
@@ -77,7 +81,6 @@ const App = () => {
                 handleMarkerClick={handleMarkerClick}
                 handleInfoWindowClose={handleInfoWindowClose}
               />
-            </Route>
           </>
         )}
       </div>
