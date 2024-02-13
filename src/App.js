87499a37476d1184682c28d7c096bb9856ce9192
Rugwed Bodhankar <rugwed07@gmail.com>
2024-02-13 17:06:14 +0530

    logic commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,88 @@
-import './App.css';
-import {GoogleLogin} from "react-google-login"
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import { GoogleLogin } from 'react-google-login';
+import axios from 'axios';
 import Map from './Map';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const [hospitals, setHospitals] = useState([]);
+  const [selectedHospital, setSelectedHospital] = useState(null);
 
-  const [loggedIn,setLoggedIn] = useState(false);
-  const [currentLocation,setCurrentLocation] = useState(null);
-  const [hospitals,setHospitals] = useState([]);
+  useEffect(() => {
+    if (loggedIn) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setCurrentLocation({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        },
+        () => null,
+        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+      );
+    }
+  }, [loggedIn]);
+
+  const handleLoginSuccess = (response) => {
+    setLoggedIn(true);
+  };
+
+  const handleLoginFailure = (response) => {
+    console.log(response);
+  };
+
+  const handleMarkerClick = (hospital) => {
+    setSelectedHospital(hospital);
+  };
+
+  const handleInfoWindowClose = () => {
+    setSelectedHospital(null);
+  };
+
+  useEffect(() => {
+    if (currentLocation) {
+      axios
+        .get(
+          `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${currentLocation.lat},${currentLocation.lng}&radius=5000&type=hospital&key=AIzaSyCsQg0fqJYUTKDyQwME9qR77gsh6eYZO8I`
+        )
+        .then((response) => {
+          setHospitals(response.data.results);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [currentLocation]);
 
   return (
-    <div className="App">
+    <Router>
+      <div className='App'>
         {!loggedIn ? (
-          <GoogleLogin 
-          clientId="YOUR_CLIENT_ID"
-          buttonText='Login With Google'
-          onSuccess={handleLoginSuccess}
-          onFailure={handleLoginFailure}
-          cookiePolicy={'single-host-origin'}
+          <GoogleLogin
+            clientId="204320152383-k8kfhplumfsjrdllug1gf83036jr4hul.apps.googleusercontent.com"
+            buttonText="Login with Google"
+            onSuccess={handleLoginSuccess}
+            onFailure={handleLoginFailure}
+            cookiePolicy={'single_host_origin'}
           />
         ) : (
-          <Map 
-
-
-
-          />
+          <>
+            <Route exact path="/">
+              <Map
+                currentLocation={currentLocation}
+                hospitals={hospitals}
+                selectedHospital={selectedHospital}
+                handleMarkerClick={handleMarkerClick}
+                handleInfoWindowClose={handleInfoWindowClose}
+              />
+            </Route>
+          </>
         )}
-    </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/Map.js
+++ src/Map.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { GoogleMap, InfoWindow, Marker } from 'react-google-maps';
 
-function Map() {
+const Map = ({ currentLocation, hospitals, selectedHospital, handleMarkerClick, handleInfoWindowClose }) => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <GoogleMap
+      defaultZoom={14}
+      defaultCenter={currentLocation}
+      center={currentLocation}
+    >
+      {hospitals.map((hospital) => (
+        <Marker
+          key={hospital.place_id}
+          position={{
+            lat: hospital.geometry.location.lat,
+            lng: hospital.geometry.location.lng,
+          }}
+          onClick={() => handleMarkerClick(hospital)}
+        />
+      ))}
+      {selectedHospital && (
+        <InfoWindow
+          position={{
+            lat: selectedHospital.geometry.location.lat,
+            lng: selectedHospital.geometry.location.lng,
+          }}
+          onCloseClick={handleInfoWindowClose}
+        >
+          <div>
+            <h3>{selectedHospital.name}</h3>
+            <p>{selectedHospital.vicinity}</p>
+          </div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  );
+};
 
-export default Map
+export default Map;
